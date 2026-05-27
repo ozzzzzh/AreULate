580459a36e2c9ca8241d644a4ef59215c15adb5b
ozzzzzh <aye_zangPrivate@proton.me>
2026-05-27 17:26:09 +0800

    1.First commit

---
+++ App.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  onLaunch() {
+    console.log('App Launch')
+  },
+  onShow() {
+    console.log('App Show')
+  },
+  onHide() {
+    console.log('App Hide')
+  }
+}
+</script>
+
+<style>
+/* 全局样式 */
+page {
+  background-color: #1a1a2e;
+  min-height: 100vh;
+}
+
+view, text {
+  box-sizing: border-box;
+}
+</style>
